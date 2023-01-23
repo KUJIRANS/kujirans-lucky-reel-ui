1a9a76b46936d3abee029ee02c3ff3fa88ddf1b5
codehans <94654388+codehans@users.noreply.github.com>
2023-01-23 18:41:54 +0000

    mainnet

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,6 +1,6 @@
-import { TESTNET, USK_TESTNET } from "kujira.js";
+import { MAINNET, USK } from "kujira.js";
 
-export const TOKEN = USK_TESTNET;
-export const NETWORK = TESTNET;
+export const TOKEN = USK;
+export const NETWORK = MAINNET;
 export const CONTRACT =
-  "kujira1uxeul6hxntl7rgwmagtrjx0hprue9eajj5r3m724xcestgsnvmaqzft36x";
+  "kujira1rt0fzu0w32ymr6cxl4867rwqfaen6mmx2k2ulqna9zmc0ft2ye7qxeetxn";
